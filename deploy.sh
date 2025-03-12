6efe24315ef46fed4cbbadec04a279597e55069e
deepanaruns <deepanaruns@gmail.com>
2025-03-12 13:00:40 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Deploy Docker container to AWS
+docker login -u deepanaruns -p dckr_pat_IYn2tJ7Tm39RwOeJp4STU4Jvb_w
 docker pull deepanaruns/dev:latest02
 docker run -d -p 80:80 --name devop-build deepanaruns/dev:latest02
