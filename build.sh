74c333810ff891d9e5671a02654d6db99e0b1650
deepanaruns <deepanaruns@gmail.com>
2025-03-12 12:59:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Build Docker image
-docker build -t devop-build .
+docker build -t deepanpro .
 
 # Push the image to Docker Hub (dev repo)
-docker tag devop-build deepanaruns/dev:latest01
-docker push deepanaruns/dev:latest01
+docker login -u deepanaruns -p dckr_pat_IYn2tJ7Tm39RwOeJp4STU4Jvb_w
+docker tag deepanpro deepanaruns/dev:latest02
+docker push deepanaruns/dev:latest02
